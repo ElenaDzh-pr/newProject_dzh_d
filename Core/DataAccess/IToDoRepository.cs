ea80644dc80eb6b49e3518daf1d305931c4c3f0e
edzhanaeva <edzhanaeva@ozon.ru>
2025-09-17 22:49:57 +0300

    second commit

--- Core/DataAccess/IToDoRepository.cs
+++ Core/DataAccess/IToDoRepository.cs
@@ -1,17 +1,16 @@
+using System.Collections.ObjectModel;
+
 namespace ProjectDz;
 
 public interface IToDoRepository
 {
-    IReadOnlyList<ToDoItem> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken);
-    //Возвращает ToDoItem для UserId со статусом Active
-    IReadOnlyList<ToDoItem> GetActiveByUserIdAsync(Guid userId, CancellationToken cancellationToken);
+    Task<IReadOnlyList<ToDoItem>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken);
+    Task<IReadOnlyList<ToDoItem>> GetActiveByUserIdAsync(Guid userId, CancellationToken cancellationToken);
     Task<ToDoItem?> GetAsync(Guid id, CancellationToken cancellationToken);
     Task AddAsync(ToDoItem item, CancellationToken cancellationToken);
-    Task UpdateAsync(Task<ToDoItem?> item, CancellationToken cancellationToken);
+    Task UpdateAsync(ToDoItem item, CancellationToken cancellationToken);
     Task DeleteAsync(Guid id, CancellationToken cancellationToken);
-    //Проверяет есть ли задача с таким именем у пользователя
-    bool ExistsByNameAsync(Guid userId, string name, CancellationToken cancellationToken);
-    //Возвращает количество активных задач у пользователя
-    bool CountActiveAsync(Guid userId, CancellationToken cancellationToken);
-    Task<ToDoItem> FindAsync(Guid userUserId, Func<object, object> func);
+    Task<bool> ExistsByNameAsync(Guid userId, string name, CancellationToken cancellationToken);
+    Task<int> CountActiveAsync(Guid userId, CancellationToken cancellationToken);
+    Task<IReadOnlyList<ToDoItem>> FindAsync(Guid userId, Func<ToDoItem, bool> predicate, CancellationToken cancellationToken);
 }
